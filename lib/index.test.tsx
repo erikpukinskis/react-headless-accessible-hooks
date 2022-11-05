9927ee24731584d94d37aa90dac93af5e21d4b3d
Erik Pukinskis <erik@snowedin.net>
2022-11-05 10:34:23 -0600

    confgen (new little tweaks)

--- lib/index.test.tsx
+++ lib/index.test.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { MyComponent } from "./";
-import { describe, it } from "vitest";
-import { render } from "@testing-library/react";
+import React from "react"
+import { MyComponent } from "./"
+import { describe, it } from "vitest"
+import { render } from "@testing-library/react"
 
 describe("MyComponent", () => {
   it("should render without errors", () => {
-    render(<MyComponent />);
-  });
-});
+    render(<MyComponent />)
+  })
+})
