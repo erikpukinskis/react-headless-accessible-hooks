bb7263ea042290b86d87506eb6d83344521983ec
Erik Pukinskis <erik@snowedin.net>
2022-11-06 09:58:39 -0700

    confgen (fix eslint config)

--- lib/index.test.tsx
+++ lib/index.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react"
 import React from "react"
 import { describe, it } from "vitest"
 
-const MyComponent = () => "hello world!"
+const MyComponent = () => <>hello world!</>
 
 describe("MyComponent", () => {
   it("should render without errors", () => {
