c3b9ca7b07eb5af2403361fa97ae065e3fa4d6ca
Erik Pukinskis <erik@snowedin.net>
2022-11-09 18:04:24 -0700

    Add a placeholder to the list

--- lib/useOrderableList.test.tsx
+++ lib/useOrderableList.test.tsx
@@ -1,6 +1,6 @@
-import { render } from "@testing-library/react"
+import { render, fireEvent, cleanup } from "@testing-library/react"
 import React from "react"
-import { describe, expect, it } from "vitest"
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
 import { useOrderableList } from "~/index"
 
 const ITEMS = [
@@ -18,30 +18,101 @@ const ITEMS = [
   },
 ]
 
-const List = () => {
-  const { getItemProps, items } = useOrderableList(ITEMS)
+describe("useOrderableList", () => {
+  const onClick = vi.fn()
 
-  return (
-    <ul>
-      {items.map(({ id, text }) => (
-        <li key={id} {...getItemProps(id)}>
-          {text}
-        </li>
-      ))}
-    </ul>
-  )
-}
+  const List = () => {
+    const { getItemProps, items, isPlaceholder } = useOrderableList(ITEMS)
 
-describe("MyComponent", () => {
-  it("should render without errors", () => {
+    return (
+      <ul>
+        {items.map((item) =>
+          isPlaceholder(item) ? (
+            <li key={item.key}>Placeholder</li>
+          ) : (
+            <li key={item.id} onClick={onClick} {...getItemProps(item.id)}>
+              {item.text}
+            </li>
+          )
+        )}
+      </ul>
+    )
+  }
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  afterEach(() => {
+    cleanup()
+  })
+
+  it("example render without errors", () => {
     render(<List />)
   })
 
-  it("should show the first one first", () => {
+  it("example should show the first one first", () => {
     const { getAllByRole } = render(<List />)
 
     const items = getAllByRole("listitem")
 
     expect(items[0].innerHTML).toEqual("First one")
+    expect(items).toHaveLength(3)
+  })
+
+  it("should fire a click event if we mousedown without dragging", () => {
+    const { getAllByRole } = render(<List />)
+
+    const [first] = getAllByRole("listitem")
+
+    fireEvent.mouseEnter(first, {
+      clientX: 0,
+      clientY: 0,
+    })
+
+    fireEvent.mouseDown(first, {
+      clientX: 10,
+      clientY: 10,
+    })
+
+    fireEvent.mouseUp(first, {
+      clientX: 10,
+      clientY: 10,
+    })
+
+    fireEvent.click(first, {
+      clientX: 10,
+      clientY: 10,
+    })
+
+    expect(onClick).toHaveBeenCalledOnce()
+  })
+
+  it("should pop the element out and add a placeholder if we drag", () => {
+    const { getAllByRole } = render(<List />)
+
+    const [first] = getAllByRole("listitem")
+
+    fireEvent.mouseDown(first, {
+      clientX: 10,
+      clientY: 10,
+    })
+
+    fireEvent.mouseMove(first, {
+      clientX: 20,
+      clientY: 20,
+    })
+
+    fireEvent.mouseUp(first, {
+      clientX: 20,
+      clientY: 20,
+    })
+
+    expect(onClick).not.toHaveBeenCalled()
+
+    const items = getAllByRole("listitem")
+
+    expect(items).toHaveLength(4)
+    expect(items[1].innerHTML).toEqual("Placeholder")
   })
 })
--- lib/useOrderableList.ts
+++ lib/useOrderableList.ts
@@ -1,14 +1,118 @@
+import type React from "react"
+import { useState, useRef, useMemo } from "react"
+import short from "short-uuid"
+
 type ObjectWithId = {
   id: string
 }
 
+type Placeholder = {
+  __typename: "Placeholder"
+  // rect: PlaceholderRect
+  key: string
+}
+
+const isPlaceholder = (
+  item: Placeholder | ObjectWithId
+): item is Placeholder => {
+  return (item as Placeholder).__typename === "Placeholder"
+}
+
+class OrderableListState {
+  downId: string | undefined
+  downAt: { x: number; y: number } | undefined
+  draggingId: string | undefined
+
+  setDown(id: string, event: React.MouseEvent) {
+    this.downId = id
+    this.downAt = { x: event.clientX, y: event.clientY }
+  }
+
+  isDrag(id: string, event: React.MouseEvent) {
+    // If we didn't mouse down on a draggable element, this is definitely not a drag
+    if (!this.downId || !this.downAt) return false
+    // If we moused down on a different draggable element, that's weird. Not a drag.
+    if (id !== this.downId) return false
+    // If we came up at the same pixel we went down, also not a drag.
+    if (event.clientX === this.downAt.x && event.clientY === this.downAt.y)
+      return false
+    // Otherwise, yes it is a drag!
+    return true
+  }
+
+  // startDrag() {
+  //   this.draggingId = this.downId
+  // }
+}
+
+type ItemProps = Pick<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>,
+  "onMouseDown" | "onMouseUp" | "onMouseMove"
+>
+
 export const useOrderableList = <ItemType extends ObjectWithId>(
   items: ItemType[]
 ) => {
-  const getItemProps = (id: string) => ({})
+  const [list] = useState(() => new OrderableListState())
+  const [placeholderIndex, setPlaceholderIndex] = useState(-1)
+  const [draggingId, setDraggingId] = useState<string | undefined>()
+  // const itemPropsCache = useRef<Record<string, ItemProps>>({})
+
+  const itemsAndPlaceholders = useMemo(
+    function insertPlaceholders() {
+      const itemsToSplice = items
+
+      if (placeholderIndex < 0) return items
+
+      // if (!placeholderRect) return items
+
+      const before = itemsToSplice.slice(0, placeholderIndex)
+      const after = itemsToSplice.slice(placeholderIndex)
+      const placeholder: Placeholder = {
+        __typename: "Placeholder",
+        key: short.generate(),
+        // rect: placeholderRect,
+      }
+
+      const newItems = [...before, placeholder, ...after]
+
+      return newItems
+    },
+    [items, placeholderIndex]
+  )
+
+  const getItemProps = (id: string) => {
+    // if (itemPropsCache.current[id]) return itemPropsCache[id]
+
+    const props: ItemProps = {
+      onMouseDown: (event: React.MouseEvent) => {
+        list.setDown(id, event)
+      },
+      // onMouseUp: (event: React.MouseEvent) => {
+      // },
+      onMouseMove: (event: React.MouseEvent) => {
+        if (!list.isDrag(id, event)) return
+        startDrag()
+      },
+    }
+
+    const startDrag = () => {
+      const draggingItemIndex = items.findIndex(
+        (item) => item.id === list.downId
+      )
+      setPlaceholderIndex(draggingItemIndex + 1)
+      // list.startDrag()
+      setDraggingId(id)
+    }
+
+    // itemPropsCache.current[id] = props
+
+    return props
+  }
 
   return {
     getItemProps,
-    items,
+    items: itemsAndPlaceholders,
+    isPlaceholder,
   }
 }
