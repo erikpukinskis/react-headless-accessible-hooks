bc249e27d8b4f55f031696a2a6877480d03c92c8
Erik Pukinskis <erik@snowedin.net>
2023-05-03 01:48:57 -0600

    confgen (Add lib build)

---
+++ vite.lib.config.js
@@ -0,0 +1,42 @@
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+  },
+
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./lib"),
+    },
+  },
+
+  plugins: [react()],
+
+  build: {
+    emptyOutDir: false,
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "RHAH",
+      fileName: (format) => `lib.${format}.js`,
+    },
+
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["lodash", "short-uuid", "react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          "lodash": "lodash",
+          "short-uuid": "shortuuid",
+          "react": "react",
+        },
+      },
+    },
+  },
+})
