ec8973f54c6f99396836de1e82046e1acb92d44c
Erik Pukinskis <erik@snowedin.net>
2023-05-11 14:28:25 -0600

    Export isAddOption

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,2 @@
 export { useOrderedTree, useOrderedTreeNode } from "./OrderedTree"
-export { useSelect, useAutocomplete } from "./Select"
+export { useSelect, useAutocomplete, isAddOption } from "./Select"
